62520af8ecd6ad81edb42b48c2a4893f7c05d53a
Dokker24 <andruhasapkin@gmail.com>
2026-04-21 01:32:49 +0300

    clean

--- userland/target/debug/deps/userland-7ccf3ed0280084da.d
+++
@@ -1,5 +0,0 @@
-/home/sysitn/Desktop/project/SysitnOS/userland/target/debug/deps/userland-7ccf3ed0280084da.d: src/main.rs
-
-/home/sysitn/Desktop/project/SysitnOS/userland/target/debug/deps/libuserland-7ccf3ed0280084da.rmeta: src/main.rs
-
-src/main.rs:
